3a4bdda4fc2dc7485f5d72a117f7586466ac57cb
Karan Kyanam <karan.kyanam@gmail.com>
2015-01-07 07:21:38 +0000

    added devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to the Rails logger.
+  # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.perform_deliveries = true
+
 end
