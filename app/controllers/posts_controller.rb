288269412d01c6df3d0bae5b86c46f8aa1e6c189
Karan Kyanam <karan.kyanam@gmail.com>
2015-01-14 19:37:38 +0000

    Topics and nesting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,25 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-    authorize @posts
-  end
+  
 
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
   end
 
   def new
     @post = Post.new
+    @topic = Topic.find(params[:topic_id])
     authorize @post
   end
   
   
    def create
      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+     @topic = Topic.find(params[:topic_id])
      authorize @post
      if @post.save
        flash[:notice] = "Post was saved."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :new
@@ -29,15 +29,17 @@ class PostsController < ApplicationController
 
   def edit
      @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
     authorize @post
   end
 
    def update
      @post = Post.find(params[:id])
+     @topic = Topic.find(params[:topic_id])
      authorize @post
      if @post.update_attributes(params.require(:post).permit(:title, :body))
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,14 @@
    user.save!
  end
  users = User.all
- 
+
+  15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
  # Note: by calling `User.new` instead of `create`,
  # we create an instance of User which isn't immediately saved to the database.
  
@@ -20,7 +27,8 @@
 # Create Posts
 50.times do
   Post.create!(
-     user:   users.sample,
+    user:   users.sample,
+     topic:  topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
@@ -36,12 +44,35 @@ posts = Post.all
   )
 end
 
-user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'karan.kyanam@gmail.com',
+
+
+admin = User.new(
+   name:     'Karan',
+   email:    'karan.kyanam@gmail.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator Karan',
+   email:    'karan.kyanam@yahoo.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member Karan',
+   email:    'invokh@gmail.com',
    password: 'helloworld'
  )
+ member.skip_confirmation!
+ member.save!
 
 puts "Seed finished"
 puts "#{User.count} users created"
